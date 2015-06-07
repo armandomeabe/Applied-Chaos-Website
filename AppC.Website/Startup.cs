2646cf259124c4c946946bf8b709ecd64a73e9e9
Armando Andrés Meabe <armando.meabe@outlook.com>
2015-06-07 05:40:20 -0300

    Commit Inicial

---
+++ AppC.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppC.Website.Startup))]
+namespace AppC.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
